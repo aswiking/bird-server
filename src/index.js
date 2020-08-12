de3f2cba4f6498ac9852d3f16b82f094f67bc4ab
Sofia Wiking <aswiking@gmail.com>
2020-08-12 12:07:34 +0100

    added 404 handling

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ app.use(express.json());
 
 app.use(birdRouter);
 
+app.use((req, res, next) => {
+    throw {
+        status: 404,
+        messages: ["Not found"]
+    };
+});
+
 app.use(errorHandlingMW);
 
 app.listen(8080, () => {
