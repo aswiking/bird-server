7c657d8c4e867305f8e5e7ef90ef5318a5376920
Sofia Wiking <aswiking@gmail.com>
2020-11-11 18:35:04 +0000

    merged get requests for bird server and fixed typo

--- src/routers/bird-router.js
+++ src/routers/bird-router.js
@@ -13,17 +13,20 @@ router.get(
 
     if (req.query.query) {
       const { rows: birds } = await db.query(
-        `SELECT birds.id, birds.common, birds.scientific, groups.id AS group_id, groups.name AS group_name
+        `SELECT birds.id, birds.common, birds.scientific, birds.uk_status, groups.id AS group_id, groups.name AS group_name, groups.scientific AS group_scientific
             FROM birds JOIN groups ON (birds.group_id = groups.id)
             WHERE birds.common ILIKE $1
             OR birds.scientific ILIKE $1
-            OR groups.name ILIKE $1`,
+            OR groups.name ILIKE $1
+            ORDER BY groups.id DESC`,
         [`%${req.query.query}%`]
       );
       res.status(200).json(birds.map(hydrateBird));
     } else {
       const { rows: birds } = await db.query(
-        "SELECT id, group_id, common, scientific FROM birds"
+        `SELECT birds.id, birds.common, birds.scientific, birds.uk_status, groups.id AS group_id, groups.name AS group_name, groups.scientific AS group_scientific
+        FROM birds JOIN groups ON (birds.group_id = groups.id)
+        ORDER BY groups.id DESC`
       );
      
       res.status(200).json(birds);
