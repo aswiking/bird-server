5f8ac7324cdb07239e795c6eca28d7ceb04c13e6
Sofia Wiking <aswiking@gmail.com>
2020-12-14 15:19:43 +0000

    added endpoint for individual species

--- src/routers/bird-router.js
+++ src/routers/bird-router.js
@@ -18,7 +18,7 @@ router.get(
             WHERE birds.common ILIKE $1
             OR birds.scientific ILIKE $1
             OR groups.name ILIKE $1
-            ORDER BY groups.id DESC`,
+            ORDER BY groups.id ASC`,
         [`%${req.query.query}%`]
       );
       res.status(200).json(birds.map(hydrateBird));
@@ -26,7 +26,7 @@ router.get(
       const { rows: birds } = await db.query(
         `SELECT birds.id, birds.common, birds.scientific, birds.uk_status, groups.id AS group_id, groups.name AS group_name, groups.scientific AS group_scientific, sightings.id AS sighting_ids
         FROM birds JOIN groups ON (birds.group_id = groups.id) LEFT JOIN sightings ON (birds.id = sightings.bird_id)
-        ORDER BY groups.id DESC`
+        ORDER BY groups.id ASC`
       );
 
 
@@ -58,5 +58,65 @@ router.get(
     }
   })
 );
+
+router.get(
+  "/api/birds/:birdID",
+  requireLogin,
+  wrapAsync(async (req, res) => {
+    const { rows: birdDetails } = await db.query(
+      `SELECT birds.id, birds.common, birds.scientific, birds.uk_status, 
+        groups.id AS group_id, groups.name AS group_name, groups.scientific AS group_scientific, 
+        sightings.id AS sighting_id,sightings.datetime, sightings.lat, sightings.lng,  sightings.notes,
+        photos.id as photo_id, photos.instagram_media_id
+        FROM birds JOIN groups ON (birds.group_id = groups.id) LEFT JOIN sightings ON (birds.id = sightings.bird_id) LEFT JOIN photos ON (photos.sighting_id = sightings.id)
+        WHERE birds.id = $1`,
+        [req.params.birdID]
+     )
+
+
+     const birdObject = {
+       id: birdDetails[0].id,
+       common: birdDetails[0].common,
+       scientific: birdDetails[0].scientific,
+       uk_status: birdDetails[0].uk_status,
+       group_name: birdDetails[0].group_name,
+       group_scientific: birdDetails[0].group_scientific
+     }
+
+
+     let sightingsObject = {};
+
+     birdDetails.forEach((sightingRow, index) => {
+
+      if (!(sightingRow.sighting_id in sightingsObject)) {
+
+        sightingsObject[sightingRow.sighting_id] = {
+          id: sightingRow.sighting_id,
+          user_id: sightingRow.user_id,
+          datetime: sightingRow.datetime,
+          lat: sightingRow.lat,
+          lng: sightingRow.lng,
+          notes: sightingRow.notes,
+          photos: [
+            {
+              photo_id: sightingRow.photo_id,
+              instagram_media_id: sightingRow.instagram_media_id,
+            },
+          ],
+        }
+      } else {
+        sightingsObject[sightingRow.sighting_id].photos.push({
+          photo_id: sightingRow.photo_id,
+          instagram_media_id: sightingRow.instagram_media_id,
+        })
+      }
+
+     })
+
+
+
+res.status(200).json(sightingsObject)
+  })
+)
 
 export default router;
