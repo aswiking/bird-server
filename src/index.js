dea0f760d4f537d44f7c93d5aed6cce3c78a26f4
Sofia Wiking <aswiking@gmail.com>
2021-10-21 13:36:02 +0100

    set port when in production mode

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import express from 'express';
-import https from 'https';
-import './firebase.js'
-import sightingsRouter from './routers/sightings-router.js';
-import birdRouter from './routers/bird-router.js';
-import loginRouter from './routers/login-router.js';
-import errorHandlingMW from './error-handling-mw.js';
-import fs from'fs';
+import express from "express";
+import https from "https";
+import "./firebase.js";
+import sightingsRouter from "./routers/sightings-router.js";
+import birdRouter from "./routers/bird-router.js";
+import loginRouter from "./routers/login-router.js";
+import errorHandlingMW from "./error-handling-mw.js";
+import fs from "fs";
 
 const app = express();
 
 let server;
 
 if (process.env.HTTPS) {
-const key = fs.readFileSync('key.pem');
-const cert = fs.readFileSync('cert.pem');
+  const key = fs.readFileSync("key.pem");
+  const cert = fs.readFileSync("cert.pem");
 
- server = https.createServer({key: key, cert: cert }, app);
-} else { server = app;}
+  server = https.createServer({ key: key, cert: cert }, app);
+} else {
+  server = app;
+}
 
 app.use(express.json());
 
@@ -27,17 +29,20 @@ app.use(sightingsRouter);
 app.use(birdRouter);
 
 app.use((req, res, next) => {
-    throw {
-        status: 404,
-        messages: ["Not found"]
-    };
+  throw {
+    status: 404,
+    messages: ["Not found"],
+  };
 });
 
 app.use(errorHandlingMW);
 
-server.listen(8080, () => {
-    console.log('Server is listening on port 8080')
-});
-
-
-
+if (process.env.PORT) {
+  server.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+  });
+} else {
+  server.listen(8080, () => {
+    console.log("Server is listening on port 8080");
+  });
+}
