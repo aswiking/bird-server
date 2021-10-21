4d096b346185fed4050129d3a284bafed4b66087
Sofia Wiking <aswiking@gmail.com>
2021-10-21 17:08:49 +0100

    Add SSL option to PG

--- src/db.js
+++ src/db.js
@@ -3,7 +3,10 @@ import pg from 'pg';
 const connectionString = process.env.DATABASE_URL || 'postgresql://localhost/birdbase';
 
 const pool = new pg.Pool({
-    connectionString
+    connectionString,
+    ssl: process.env.NODE_ENV === "production" ? {
+        rejectUnauthorized: false
+    } : undefined
 });
 
 export default pool;
